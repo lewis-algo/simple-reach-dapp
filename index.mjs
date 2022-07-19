6e8d7d1e6a2d84681a09b15aaf08ff9ba4095258
Lewis Algo <lewis.algo.xyz@gmail.com>
2022-07-19 18:29:15 -0500

    Got the balance of the hardcoded wallet in algos & logged balance to the console

--- index.mjs
+++ index.mjs
@@ -6,3 +6,7 @@ const stdlib = loadStdlib();
 const address = 'Y76M3MSY6DKBRHBL7C3NNDXGS5IIMQVQVUAB6MP4XEMMGVF2QWNPL226CA';
 // Log the hardcoded wallet address
 console.log(`[.mjs] Whitelisted wallet address: ${address}`)
+// Get the hardcoded wallet network token balance 
+const balance = await loadStdlib(process.env).balanceOf(address);
+// Log the hardcoded wallet network token balance
+console.log(`[.mjs] Whitelisted wallet network token balance: ${balance} ALGO`);
